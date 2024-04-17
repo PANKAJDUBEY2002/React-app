60f1f7629dc9c50af236a1988b1489f8e3296944
PANKAJ DUBEY <bt21cse197@iiitn.ac.in>
2024-04-17 17:14:24 +0530

    integrate tailwind in css and props in react

---
+++ src/App1.jsx
@@ -0,0 +1,15 @@
+import Card from "./Card"
+function App1() {
+    
+  
+    return (
+      <>
+       <h1 className="bg-green-400 rounded-xl text-black p-4">Tailwind Test</h1>
+       <Card material="utility jacket" />
+       <Card material="utility iphone" />
+    
+      </>
+    )
+  }
+  
+  export default App1
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Chai from './Chai.jsx'
+import App1 from './App1.jsx'
+import Card from './Card.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Chai/>
-    <App />
+  
+    <App1 />
+    
   </React.StrictMode>,
 )
